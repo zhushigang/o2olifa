8fa2a374c3cacfbb2ad3b5cba502841c8fd85e77
fyang0321 <fyang880321@gmail.com>
2016-05-04 13:13:51 -0500

    add more attributes

--- ubarber/scheduler/models.py
+++ ubarber/scheduler/models.py
@@ -1,3 +1,30 @@
+# from django.db import models
+
+# # Create your models here.
+
+# class Order(models.Model):
+#     MALE = 'M'
+#     FEMALE = 'F'
+#     GENDER_CHOICES = (
+#         (MALE, 'MALE'),
+#         (FEMALE, 'FEMALE'),
+#     )
+#     first_name = models.CharField(max_length=50)
+#     last_name = models.CharField(max_length=50)
+#     gender = models.CharField(max_length=2,
+#                                       choices=GENDER_CHOICES,
+#                                       default=MALE)
+#     mobile_number = models.CharField(max_length=10)
+#     address1 = models.CharField(max_length=200)
+#     address2 = models.CharField(max_length=200)
+#     date = models.DateField('time of date');
+#     time = models.TimeField('time of service')
+
+# class ServiceSlot(models.Model):
+#     date = models.DateField('service date')
+#     time = models.DateTimeField('service start time')
+#     available = models.BooleanField()
+
 from django.db import models
 
 # Create your models here.
